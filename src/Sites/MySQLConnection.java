a7a24e84653fa93b416415173b5c913100254e36
kggrinch <kirillkongrichanichenko@gmail.com>
2024-08-10 18:34:19 -0700

    Update MySQLConnection.java

--- src/Sites/MySQLConnection.java
+++ src/Sites/MySQLConnection.java
@@ -8,7 +8,7 @@ package Sites;
  *****************************************************************************/
 public interface MySQLConnection {
     public static final String DB_URL = "jdbc:mysql://localhost:3306/sitesDB";
-    public static final String USER = "DB_USER"; // Fill information with your local db
-    public static final String PASS = "DB_PASS"; // Fill information with you local db
+    public static final String USER = "DB_USER"; // Fill user name from your database server
+    public static final String PASS = "DB_PASS"; // Fill password from your database server
     public static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
 }
